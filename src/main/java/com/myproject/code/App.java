dd8812264f2689acc91cdbe5edf9ee4075d0bc98
Vinyasa <vinyasa.mv@gmail.com>
2021-09-25 12:04:24 +0530

    first commit

---
+++ src/main/java/com/myproject/code/App.java
@@ -0,0 +1,13 @@
+package com.myproject.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String sentence()
+    {
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/myproject/code/AppTest.java
@@ -0,0 +1,21 @@
+package com.myproject.code;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       
+       assertEquals("Helo World!",App.sentence());
+    }
+}
